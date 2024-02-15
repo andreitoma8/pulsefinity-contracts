42f6924a2f07b96b2dc61c5f497959e8820df961
kash-cytric <kash@cytric.io>
2024-02-15 11:28:18 +0100

    latest deployment

--- helper-config.ts
+++ helper-config.ts
@@ -4,7 +4,7 @@ const toWei = (amount: string) => ethers.utils.parseEther(amount);
 
 const helperconfig = {
     // The address of the Pulsefinity token
-    pulsefinityAddress: "0xe6Eea3fC1b0B850C888C26cf803Ab862A9cC481B",
+    pulsefinityAddress: "0x41Ff71a99D7744cc5e62418d59774e19e30F7426",
     // The amount of tokens needed to be staked to reach each tier
     // Only change the values in quotes
     tierLimits: {
